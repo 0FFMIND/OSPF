c41ed766a712472fb3ad12e38d421156d69eb771
OFFMIND <92588712+0FFMIND@users.noreply.github.com>
2024-12-09 16:20:38 -0500

    Create test_sender.cpp

---
+++ test_sender.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "udp_socket.h"
+#include "OSPFHello.h"
+
+int main() {
+    
+    UDPSocket socket(0);
+
+    OSPFHello hello = {
+        0xFFFFFF00, // networkMask
+        10,         // helloInterval
+        1,          // routerPriority
+        40,         // deadInterval
+        0,          // designatedRouter
+        0,          // backupDesignatedRouter
+        {0x05060708} // neighbors
+    };
+    
+    socket.sendHello("224.0.0.5", 1050, hello);
+    std::cout << "Hello packet sent." << std::endl;
+
+    return 0;
+}
